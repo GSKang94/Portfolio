95acfa863c03d886443ab01bd09cff8ca4ab4837
GSingh1994 <singh.gagandeep0034@gmail.com>
2020-11-23 22:38:47 -0600

    new style on scroll

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,24 @@
+const navbar = document.querySelector('#navbar');
+window.onscroll = () => {
+    if (window.scrollY > 530 && window.scrollY <= 1000) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
+
+console.log('attached')
+
+
+
+
+
 // dark mode 
 // Select the button
-const btn = document.querySelector('.btn-toggle');
+// const btn = document.querySelector('.btn-toggle');
 // Listen for a click on the button
-btn.addEventListener('click', function () {
+// btn.addEventListener('click', function () {
     // Then toggle (add/remove) the .dark-theme class to the body
-    document.body.classList.toggle('dark-theme');
-})
+    // document.body.classList.toggle('dark-theme');
+// })
 // dark mode end
