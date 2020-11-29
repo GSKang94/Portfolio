eb61486a309f5f3bb9521ef3d20cbc4dd330bab2
GSingh1994 <singh.gagandeep0034@gmail.com>
2020-11-28 18:23:07 -0600

    added animation to top bar and filled out anchors

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector('#navbar');
 window.onscroll = () => {
-    if (window.scrollY > 730) {
+    if (window.scrollY > 910) {
         navbar.classList.add('nav-active');
     } else {
         navbar.classList.remove('nav-active');
