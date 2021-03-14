64127c5bd87d508e4e62095c5a774f2864e08e0c
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-13 22:57:45 -0600

    Fixed transitions

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,12 @@
-const navbar = document.querySelector('#navbar');
-
+const navbar = document.querySelector("#navbar");
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        navbar.style.top = "0";
-    } else {
-        navbar.style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
-}
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
